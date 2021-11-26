c216aa5dd20aafb7e19cacc51a050817e7a95ba5
Jing Wei Li <ljw9801055@gmail.com>
2021-11-26 18:46:01 -0500

    Initial Commit

---
+++ Shared/GroupableContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupableContactsApp.swift
+//  Shared
+//
+//  Created by Jing Wei Li on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct GroupableContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
